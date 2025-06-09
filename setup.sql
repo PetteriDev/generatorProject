f8b70d444997122d3c46761e51b2ef120232ec96
Your Name <you@example.com>
2025-06-09 22:41:30 +0300

    Update

--- setup.sql
+++ setup.sql
@@ -2,14 +2,16 @@ CREATE TABLE cramo_generators (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     run_id INTEGER NOT NULL,
-    scraped_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    scraped_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    photo_url TEXT
 );
 
 CREATE TABLE ramirent_generators (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     run_id INTEGER NOT NULL,
-    scraped_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    scraped_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    photo_url TEXT
 );
 
 CREATE SEQUENCE scrape_run_id_seq START 1;
